afbcd021a1318bc20400b16c2153e5b24597b442
Will <will_2235@hotmail.com>
2019-12-13 16:53:24 +0000

    actual bug fixes

--- stockprices/forms.py
+++ stockprices/forms.py
@@ -5,6 +5,6 @@ class PostForm(forms.Form):
     start = forms.DateField(label = "Start (yyyy-mm-dd):")
     end = forms.DateField(label = "End (yyyy-mm-dd):")
     tickers = forms.CharField(widget=forms.Textarea, label = "(Yahoo) Tickers (separated by spaces):")
-    periodicity = forms.ChoiceField(choices=[("1mo",'Monthly'), ("1w", 'Weekly'), ("1d", 'Daily')])
+    periodicity = forms.ChoiceField(choices=[("1mo",'Monthly'), ("1wk", 'Weekly'), ("1d", 'Daily')])
 
 
