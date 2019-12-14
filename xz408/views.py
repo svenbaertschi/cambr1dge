f86b44349a4cdd2377a6304c1744d04ec5dd9558
Will <will_2235@hotmail.com>
2019-12-14 01:11:02 +0000

    clarifying

--- xz408/views.py
+++ xz408/views.py
@@ -7,7 +7,7 @@ def apps(request):
     return render(request, 'temptext.html', {'message':  "<h1>uhh too lazy to reupload</h1>"})
 
 def developers(request):
-    return render(request, 'temptext.html', {'message':  "<a href='https://github.com/svenbaertschi/mako' class='link-dark'>github (help here)</a>"})
+    return render(request, 'temptext.html', {'message':  "<a href='https://github.com/svenbaertschi/cambr1dge' class='link-dark'>github (help here)</a>"})
 
 def contact(request):
     return render(request, 'temptext.html', {'message':  "<a href='mailto:will@ubctacs.org' class='link-dark'>will@ubctacs.org</a>"})
